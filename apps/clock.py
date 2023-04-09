ba0bdd3f2555d20eaa89ce16904b8e8cd400dc85
GamePlayer-8 <gameplayer2019pl@tutamail.com>
2023-04-09 11:17:22 +0200

    PYLINT: fix clock.py

--- apps/clock.py
+++ apps/clock.py
@@ -1,21 +1,28 @@
-import datetime, time
+"""
+GNU GPL 3.0
+Mizu /edits by Chimmie
+"""
+
+import time
+import datetime
 from blessed import Terminal
-import modules._7sd as _7sd
-import modules.progress_bar as progress
+from modules import _7sd
+from modules import progress_bar as progress
 
 term = Terminal()
 
 display = _7sd.SevenSegmentDisplay()
 
-displays_x = []
-
 def convert_state(string):
-    li = []
+    """
+        AI:define_function # Needs work
+    """
+    line = []
     for char in string:
         if char in (' ', '\t', '\r', '\n'):
             continue
-        li.append(char == '#')
-    return li
+        line.append(char == '#')
+    return line
 
 states = [
     convert_state("""
@@ -90,46 +97,97 @@ convert_state("""
 """),
 ]
 
-def update(dt, frame, keys):
-    global displays_x
+def calc_time(now):
+    """
+        AI:define_function # Needs work
+    """
+    hour = '0' + str(now.hour) if now.hour < 10 else str(now.hour)
+    minute = '0' + str(now.minute) if now.minute < 10 else str(now.minute)
+    second = '0' + str(now.second) if now.second < 10 else str(now.second)
+    return [hour, minute, second]
+
+def update(_dt, _frame, _keys): # PLEASE SPECIFY THE USAGE OF IT
+    """
+        AI:define_function # Needs work
+    """
+    displays_x = []
     render_str = ''
 
     now = datetime.datetime.now()
     sec_time = time.time()
-    hr = '0' + str(now.hour) if now.hour < 10 else str(now.hour)
-    mn = '0' + str(now.minute) if now.minute < 10 else str(now.minute)
-    sec = '0' + str(now.second) if now.second < 10 else str(now.second)
 
-    digits = hr + mn + sec
+    digits = calc_time(now)
+    digits = digits[0] + digits[1] + digits[2]
     size = 3
-    x = int((term.width-78)//2)
-    y = 5
+    x_position = int((term.width-78)//2)
+    y_position = 5
 
     off_x = 0
-    for i in range(len(digits)):
-        digit = digits[i]
+    for i, digit in enumerate(digits):
         num_digit = int(digit)
 
         display.state = states[num_digit]
-        render_str += display.render(x + i * size * 4 + off_x * 4, y, size=size)
-        if i%2 == 1:
+        render_str += display.render(x_position + i * size *
+                                     4 + off_x * 4, y_position, size=size)
+        if i % 2 == 1:
             off_x += 1
         else:
-            displays_x.append(x + i * size * 4 + off_x * 4)
+            displays_x.append(x_position + i * size * 4 + off_x * 4)
 
     blink_interval = 1
     blink_length = 0.5
 
     if sec_time % blink_interval < blink_length:
         for i in range(2):
-            render_str += term.move_xy(x + ((size + 2)*4 + 4)*(i+1) + (4 if i == 1 else 0), y + 2) + term.on_white('  ')
-            render_str += term.move_xy(x + ((size + 2)*4 + 4)*(i+1) + (4 if i == 1 else 0), y + 6) + term.on_white('  ')
+            render_str += term.move_xy(
+                x_position + ((size + 2)*4 + 4)*(i+1) + (4 if i == 1 else 0),
+                y_position + 2
+            ) + term.on_white('  ')
+            render_str += term.move_xy(
+                x_position + ((size + 2)*4 + 4)*(i+1) + (4 if i == 1 else 0),
+                y_position + 6
+            ) + term.on_white('  ')
 
-    render_str += term.move_xy(displays_x[0], y - 2) + term.red(progress.definite(2*(4 + size * 2) + 2, now.hour/24))
-    render_str += term.move_xy(displays_x[1], y - 2) + term.orange(progress.definite(2*(4 + size * 2) + 2, now.minute/60))
-    render_str += term.move_xy(displays_x[2], y - 2) + term.cyan(progress.definite(2*(4 + size * 2) + 2, now.second/60))
-    render_str += term.move_xy(displays_x[2], y - 3) + term.blue(progress.definite(2*(4 + size * 2) + 2, sec_time%1))
-    
-    render_str += term.move_xy(x, y + 10) + f'{now.day}/{now.month}/{now.year}'
+    render_str += term.move_xy(
+        displays_x[0],
+        y_position - 2
+    ) + term.red(
+        progress.definite(
+            2*(4 + size * 2) + 2,
+            now.hour/24
+        )
+    )
+    render_str += term.move_xy(
+        displays_x[1],
+        y_position - 2
+    ) + term.orange(
+        progress.definite(
+            2*(4 + size * 2) + 2,
+            now.minute/60
+        )
+    )
+    render_str += term.move_xy(
+        displays_x[2],
+        y_position - 2
+    ) + term.cyan(
+        progress.definite(
+            2*(4 + size * 2) + 2,
+            now.second/60
+        )
+    )
+    render_str += term.move_xy(
+        displays_x[2],
+        y_position - 3
+    ) + term.blue(
+        progress.definite(
+            2*(4 + size * 2) + 2,
+            sec_time % 1
+        )
+    )
+
+    render_str += term.move_xy(
+        x_position,
+        y_position + 10
+    ) + f'{now.day}/{now.month}/{now.year}'
 
     return render_str
