f165d8af9c61af81a4c8d945f7dae8584f26111c
GamePlayer-8 <gameplayer2019pl@tutamail.com>
2023-04-09 17:38:26 +0200

    PYLINT: Fix _7sd.py.

--- modules/_7sd.py
+++ modules/_7sd.py
@@ -1,37 +1,71 @@
+"""
+GNU GPL 3.0
+Mizu /edits by Chimmie
+"""
+
 # 7-segment display
 from blessed import Terminal
-term = Terminal()
+TERM = Terminal()
 
 class SevenSegmentDisplay:
+    """
+        AI:define_function # Needs work
+    """
     def __init__(self, state=[True for i in range(7)]):
+        """
+            AI:define_function # Needs work
+        """
         self.state = state
 
-    def render_hori_line(self, x, y, length):
+    def render_hori_line(self, x_position, y_position, length):
+        """
+            AI:define_function # Needs work
+        """
         string = ''
-        string += term.move_xy(x + 2, y)
+        string += TERM.move_xy(x_position + 2, y_position)
         string += ' ' * (length * 2)
         return string
 
-    def render_vert_line(self, x, y, length):
+    def render_vert_line(self, x_position, y_position, length):
+        """
+            AI:define_function # Needs work
+        """
         string = ''
         for i in range(length):
-            string += term.move_xy(x, y + i) + '  '
+            string += TERM.move_xy(x_position, y_position + i) + '  '
         return string
 
-    def render(self, x, y, colour = term.on_white, size = 3):
+    def render(self, x_position, y_position, colour = TERM.on_white, size = 3):
+        """
+            AI:define_function # Needs work
+        """
         string = ''
         if self.state[0]:
-            string += colour + self.render_hori_line(x, y, size) + term.normal
+            string += colour + \
+                self.render_hori_line(x_position, y_position, size) + \
+                TERM.normal
         if self.state[1]:
-            string += colour + self.render_vert_line(x, y + 1, size) + term.normal
+            string += colour + \
+                self.render_vert_line(x_position, y_position + 1, size) + \
+                TERM.normal
         if self.state[2]:
-            string += colour + self.render_vert_line(x + 2 + size*2, y + 1, size) + term.normal
+            string += colour + \
+                self.render_vert_line(x_position + 2 + size*2, y_position + 1, size) + \
+                TERM.normal
         if self.state[3]:
-            string += colour + self.render_hori_line(x, y + 1 + size, size) + term.normal
+            string += colour + \
+                self.render_hori_line(x_position, y_position + 1 + size, size) + \
+                TERM.normal
         if self.state[4]:
-            string += colour + self.render_vert_line(x, y + 2 + size, size) + term.normal
+            string += colour + \
+                self.render_vert_line(x_position, y_position + 2 + size, size) + \
+                TERM.normal
         if self.state[5]:
-            string += colour + self.render_vert_line(x + 2 + size*2, y + 2 + size, size) + term.normal
+            string += colour + \
+                self.render_vert_line(x_position + 2 + size*2, y_position + 2 + size, size) + \
+                TERM.normal
         if self.state[6]:
-            string += colour + self.render_hori_line(x, y + 2 + size*2, size) + term.normal
+            string += colour + \
+                self.render_hori_line(x_position, y_position + 2 + size*2, size) + \
+                TERM.normal
         return string
