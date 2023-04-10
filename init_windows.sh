db47573efb0bb480587f0a4451c8ea8adc658518
GamePlayer-8 <gameplayer2019pl@tutamail.com>
2023-04-10 15:31:56 +0200

    wineserver modifications.

--- init_windows.sh
+++ init_windows.sh
@@ -41,11 +41,11 @@ export WINEDLLOVERRIDES="winemenubuilder.exe,mscoree,mshtml="
 Xvfb -ac :0 -screen 0 1280x1024x24 &
 sleep 5
 
-xvfb-run wine reg add 'HKLM\Software\Microsoft\Windows NT\CurrentVersion' /v CurrentVersion /d 10.0 /f
-xvfb-run wine reg add 'HKCU\Software\Wine\DllOverrides' /v winemenubuilder.exe /t REG_SZ /d '' /f
-xvfb-run wine reg add 'HKCU\Software\Wine\DllOverrides' /v mscoree /t REG_SZ /d '' /f
-xvfb-run wine reg add 'HKCU\Software\Wine\DllOverrides' /v mshtml /t REG_SZ /d '' /f
-xvfb-run wineserver -w
+xvfb-run sh -c "wine reg add 'HKLM\Software\Microsoft\Windows NT\CurrentVersion' /v CurrentVersion /d 10.0 /f && \
+wine reg add 'HKCU\Software\Wine\DllOverrides' /v winemenubuilder.exe /t REG_SZ /d '' /f && \
+wine reg add 'HKCU\Software\Wine\DllOverrides' /v mscoree /t REG_SZ /d '' /f && \
+wine reg add 'HKCU\Software\Wine\DllOverrides' /v mshtml /t REG_SZ /d '' /f; \
+wineserver -w"
 
 wget -q https://www.python.org/ftp/python/${PYTHON_VERSION}/python-${PYTHON_VERSION}-amd64.exe{,.asc} -O /python-${PYTHON_VERSION}-amd64.exe
 wget -q https://github.com/upx/upx/releases/download/v${UPX_VERSION}/upx-${UPX_VERSION}-win64.zip -O /upx-${UPX_VERSION}-win64.zip
@@ -59,12 +59,12 @@ xvfb-run sh -c "\
   unzip upx*.zip && \
   mv -v upx*/upx.exe ${WINEPREFIX}/drive_c/windows/
 
-wine python -m pip install --upgrade setuptools wheel pip > /dev/null
-wine python -m pip install pyinstaller > /dev/null
+wine python -m pip install --upgrade setuptools wheel pip
+wine python -m pip install pyinstaller
 
 cd /source
 
-wine python -m pip install -r requirements.txt > /dev/null
+wine python -m pip install -r requirements.txt
 
 wine python -m pyinstaller -F --onefile --console \
  --additional-hooks-dir=. --add-data ./config.py;config.py --add-data ./modules/*;modules/ --add-data ./apps/*;apps/ \
